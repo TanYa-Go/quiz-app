cd0d78ae4ce2d9cec14ceed38286a22b0469c445
TanYa-Go <tanja.godinic@gmail.com>
2021-03-15 16:53:11 +0000

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
-/*
-function getQuestions(amountUserPick){
-  fetch(`https://opentdb.com/api.php?amount=${amountUserPick}`)
+
+function getQuestions() {
+  fetch("https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=multiple")
 .then(bob => bob.json())
 .then(questionsData => createQuestions(questionsData))
 }
@@ -10,10 +10,54 @@ function createQuestions(listOfQuestions){
     console.log(question.question)
   })
 }
-getQuestions(100);
+getQuestions();
+
+
+/* Function to retrieve the questions, formats them and passes them to the DOM 
+*/
+  const fetchedQuestions = fetchData(`https://opentdb.com/api.php?amount=10&category=${categoryId}&type=multiple`);
+    fetchedQuestions.then((data) => {
+        questions = data.results.map(fetchedQuestion => {
+            const formattedQuestion = {
+                question : fetchedQuestion.question,
+            };
+
+            formattedQuestion.answer = Math.floor(Math.random() * 3 ) + 1;
+            const answerChoices = [ ... fetchedQuestion.incorrect_answers];
+            answerChoices.splice(formattedQuestion.answer -1, 0, fetchedQuestion.correct_answer);
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion['choice' + (index + 1)] = choice;
+            });
+
+            return formattedQuestion;
+        });
+
+        startGame();
+        }).catch(err => {
+            errorMessageRef.innerHTML = `Oops it looks like you shouldn't get any smarter. Please refresh the page to try again.`;
+            console.error(err);
+        });
+
+
+/**
+ * Sets question counter and score to 0 
+ * Defines the questions and get new question
 */
 
-/*Dropdown menu button for selecting difficutly level*/
+const startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    scoreRef.innerText = score;
+    availableQuestions = [... questions];
+    getNewQuestion();
+    loaderRef.classList.add('hide');
+    gameRef.classList.remove('hide');
+};
+       
+
+
+/* ==========================Dropdown menu button for selecting difficutly level =================================*/
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
@@ -33,3 +77,5 @@ window.onclick = function(event) {
     }
   }
 }
+
+/* ========================== End Dropdown menu button for selecting difficutly level =================================*/
